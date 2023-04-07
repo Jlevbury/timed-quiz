b1232d0a711a9a4f28233e3ae7d60dc5e34d27f5
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-04-07 16:35:45 -0400

    initial draft of documents required for generation of the quiz page

---
+++ script.js
@@ -0,0 +1,11 @@
+/*GIVEN I am taking a code quiz
+        WHEN I click the start button - button input needed
+        THEN a timer starts and I am presented with a question- timer & question display needed
+        WHEN I answer a question- user input needed
+        THEN I am presented with another question- event listener needed
+        WHEN I answer a question incorrectly- message display wrong answer needed to display correct answer
+        THEN time is subtracted from the clock - interval needed
+        WHEN all questions are answered or the timer reaches 0 - event listener needed
+        THEN the game is over
+        WHEN the game is over
+        THEN I can save my initials and score - local storage necessary*/
